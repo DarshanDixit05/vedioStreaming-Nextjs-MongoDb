e7f5663910146b04eb09761c6a9707d94b810cec
DarshanDixit05 <darshandixit0@gmail.com>
2023-06-21 04:45:39 +0530

    cleanup

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -6,25 +6,6 @@ import { useRouter } from 'next/router';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
 import Input from '../components/Input';
-
-
-// export async function getServerSideProps(context: NextPageContext) {
-//     const session = await getSession(context);
-  
-//     if (session) {
-//       return {
-//         redirect: {
-//           destination: '/',
-//           permanent: false,
-//         }
-//       }
-//     }
-  
-//     return {
-//       props: {}
-//     }
-//   }
-
   
 
 const Auth = () =>{
